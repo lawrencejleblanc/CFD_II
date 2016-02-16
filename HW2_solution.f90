2e81cad2678dc2c9e6e6a74bdd2253f12a35784d
Lawrence LeBlanc <lawrencejleblanc@gmail.com>
2016-02-16 00:13:02 -0600

    Update the main file to open the input file, read it correctly, and write it to the screen. It is a barebones file read and write system. TODO STILL: Figure out how to comment the input file and not write read and write the comments. Finish the read and write statements and input file to include all terms to be inputted.

--- HW2_solution.f90
+++ HW2_solution.f90
@@ -5,38 +5,47 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 Program HW1
-	Use Solvers
+	!Use Solvers
 	IMPLICIT NONE
 	REAL*8, dimension(:,:), allocatable :: A
 	REAL*8, allocatable :: source(:), x(:)
-	INTEGER :: ssize = 3, i, j
+	INTEGER :: ssize = 3, i, j, OpenStatus
+	REAL*8 :: xsize, ysize, dx, dy, q_gen
 
-	!!Allocate array sizes -- note that I start the indicies at 0 and not 1 -- default is 1
-	allocate(A(0:ssize-1,0:ssize-1))
-	allocate(source(0:ssize-1))
-	allocate(x(0:ssize-1))
-
-	!Set the coefficient matrix and the source vector
-	!A = reshape((/-2, 1, 0, 1, -2, 1, 0, 1, -2/), shape(A))
+	!! Allocate array sizes -- note that I start the indicies at 0 and not 1 -- default is 1
+	!allocate(A(0:xsize-1,0:ysize-1))
+	!allocate(source(0:ysize-1))
+	!allocate(x(0:ysize-1))
 	
-	!print*, shape(A)
+	! Opens input file
+	OPEN (UNIT = 1, FILE = "input.dat", STATUS = "OLD", ACTION = "READWRITE", IOSTAT = OpenStatus)
 
-	!b = reshape((/-100, 0, -10/), shape(b))
+	IF (OpenStatus .gt. 0) STOP "** Cannot open input file **"
+	
+	! Reads values from input file
+	READ (1, *) xsize, ysize, dx, dy, q_gen
+	!7 FORMAT(1X, F5.2, /, 1X, F5.2, /, 1X, F5.2, /, 1X, F5.2, / ,1X, F5.2)
+	WRITE (*,8) "xsize =", xsize, "ysize =", ysize, "dx =", dx, "dy =", dy, "q_gen =", q_gen 
+	8 FORMAT(1X, A7, F6.2, /, 1X, A7, F6.2, /, 1X, A4, F6.2, /, 1X, A4, F6.2, /, 1X, A5, F6.2)  
+	! Set the coefficient matrix and the source vector
+	!A = reshape((/-2, 1, 0, 1, -2, 1, 0, 1, -2/), shape(A))
 
 	! Call coefficient matrix constructer subroutine
 	!CALL coefficient_construct
 
 	! Call solver suboutines
-	IF (solver .eq. 1)
-		CALL gauss_seidel(xsize, ysize, An, As, Aw, Ae, Ap, source, Wbc, Ebc, Nbc, Sbc, T)
-	ELSEIF (solver .ne. 1)
-		CALL coefficient_construct(A, source)
-		CALL LUdecomp(ssize, A)
-		CALL LUsolve(ssize, A, b)
-		        DO j =0, ssize-1
-				x(j) = source(j)
-				print *, x(j)
-		 ENDDO
-	ENDIF
+	!IF (solver .eq. 1)
+	!	CALL gauss_seidel(xsize, ysize, An, As, Aw, Ae, Ap, source, Wbc, Ebc, Nbc, Sbc, T)
+	!ELSEIF (solver .ne. 1)
+	!	CALL coefficient_construct(A, source)
+	!	CALL LUdecomp(ssize, A)
+	!	CALL LUsolve(ssize, A, b)
+	!	        DO j =0, ssize-1
+	!			x(j) = source(j)
+	!			print *, x(j)
+	!	 ENDDO
+	!ENDIF
+
+	CLOSE (UNIT =1)
 
 END PROGRAM HW1
