2f46675e834cbd882f2277a3e31f6581f8d05b38
Lawrence LeBlanc <lawrencejleblanc@gmail.com>
2016-02-19 00:04:36 -0600

    minor changes such as variable declaration and edits to subroutine arguments list made in main. coefficient_construct can create matrices for LU decomposition when all B.C. are dirichlet. TODO: make coefficient_construct work when there are at least 1 neumann B.C., check that both solution methods work, figure out input file, commenting

--- HW2_solution.f90
+++ HW2_solution.f90
@@ -1,19 +1,26 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Name: Lawrence LeBlanc
 !Date: Feb 4, 2016
 !Program name: HW2_solution.f90
-!Program description: 
+!Program description: This program solves the 2-D steady state heat equation with no generation.
+!		      The program reads in mesh parameters and B.C. from an input file and solves
+!	              the heat equation either iteratively with the gauss seidel method or directly
+!		      with LU Decomposition. Both method's algorithms are found in the solvers file.
+!		      A seperate file contains a subroutine to create the matrices needed for LU
+! 		      Decomposition. The results are written to a tecplot file for analysis.  
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 Program HW2
-	Use Solvers
+	Use Solvers, coefficient_construct
 	IMPLICIT NONE
+
+	! Variable declaration
 	REAL*8, dimension(:,:), allocatable :: A
 	REAL*8, allocatable :: b(:), x(:), T(:)
 	INTEGER :: i, j, wtype, etype, stype, ntype, OpenStatus, solver = 1
 	REAL*8 :: dx, dy, q_gen, Wbc, Ebc, Nbc, Sbc, As, An, Ae, Ap, Aw, y, xx
-	INTEGER :: xsize, ysize, area
+	INTEGER :: xsize, ysize, area, source_size
 
-	! Opens input file
+	! Opens input file and output tecplot file
 	!OPEN (UNIT = 1, FILE = "input.dat", STATUS = "OLD", ACTION = "READWRITE", IOSTAT = OpenStatus)
 	OPEN (UNIT = 2, FILE = "output.dat", ACTION = "WRITE")
 	!IF (OpenStatus .gt. 0) STOP "** Cannot open input file **"
@@ -41,7 +48,8 @@ Program HW2
 	stype = 1
 	etype = 1
 	wtype = 1
-
+	
+	! Calculate matrix coefficients
 	As = 1/(dy**2)
 	An = As
 	Aw = 1/(dx**2)
@@ -55,16 +63,19 @@ Program HW2
 	! Call solver suboutines
 	IF (solver .eq. 1)THEN
 		allocate(T(1:xsize*ysize))
+		! Initialize temperature array to 0. These values are sued as initial T_prev
 		DO i = 1, xsize*ysize, 1
 			T(i) = 0
 		ENDDO
+		
 		CALL gauss_seidel(xsize, ysize, An, As, Aw, Ae, Ap, q_gen, Wbc, Ebc, Nbc, Sbc, T, area, wtype, etype, stype, ntype)
-		 ! Write the header to the output tecplot file
+		
+		! Write the header to the output tecplot file
 		WRITE(2,*) '"X" "Y" "TEMPERATURE"'
 		WRITE(2,9) "zone I=", xsize, "J=", ysize, " SOLUTIONTIME=", 0.00, "F=POINT"
 		9 FORMAT(2X, A7, I1, 2X, A3, I1, 2X, A14, F6.3, 2X, A7)
 		
-		 ! Write the x position, y position, and temperature values to the tecplot file
+		! Write the x position, y position, and temperature values to the tecplot file
 		DO j = 1, area, 1
 			xx = (MOD(j, (xsize)) - 1)*dx
 			IF (xx .lt. 0) THEN
@@ -75,10 +86,6 @@ Program HW2
 			11 FORMAT(F10.5, 3X, F10.5, 3X, F10.5)
 		ENDDO
 		
-		!DO j = 1, area, 1
-		!	print*, T(j)
-		!ENDDO
-	
 	!ELSEIF (solver .ne. 1)THEN
 		!ALLOCATE(A(0:xsize-1,0:ysize-1))
 		!ALLOCATE(b(0:ysize-1))
--- coefficient_construct.f90
+++ coefficient_construct.f90
@@ -7,4 +7,56 @@
 !	       reads in and creates the source vector for LU decomposition.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+	SUBROUTINE coefficient_construct(A, b, Ap, Ae, An, Aw, As, q_gen, source_size, ntype, stype, wtype, etype, Wbc, Sbc, Nbc, Ebc)
+
+	IMPLICIT NONE
+
+	! Variable declaration
+	REAL*8, INTENT(IN) :: q_gen, Ap, Ae, An, Aw, As, Wbc, Sbc, Nbc, Ebc
+	INTEGER, INTENT(IN) :: xsize, ysize
+	INTEGER :: i, j				!Loop counters
+	! Source size represents the size of the source matrix for LU decomposition as well as the
+	! the number of internal unknown temperatures in the domain.
+	REAL*8, INTENT(OUT) :: source_size
+	
+!!!!!!!!WHAT SIZE DO YOU BRING IN????????
+	REAL*8, dimension(:,:), allocatable, INTENT(INOUT) :: A
+	REAL*8, allocatable, INTENT(INOUT) :: b(:) 
+	REAL*8 :: squarert
+	
+	! Compute number of unknowns to be solved for
+	source_size = (xsize*ysize) - (2*xsize) - ((ysize-2)*2)
+	
+	squarert = SQRT(source_size)
+	
+	Create coefficient and source matrices
+	allocate(A(0:source_size-1,0:source_size-1)
+	allocate(b(0:source_size-1)
+
+	DO i = 0, source_size - 1, 1
+		DO j = 0, source_size - 1, 1
+			! If all dirichlet B.C. 
+			IF (wtype.eq.1 .AND. stype.eq.1 .AND. etype.eq.1 .AND. ntype.eq.1) THEN 
+				IF(i .eq. j) THEN
+					A(i,j) = Ap
+				ELSEIF(j .eq. i+1 .AND. j .ne. squarert .AND. j .ne. (source_size -&
+				       &squarert)
+					A(i,j) = Ae
+				ELSEIF(j .eq. i-1 .AND. i .ne. squarert .AND. i .ne. (source_size -&
+                                       &squarert)
+					A(i,j) = Aw
+				ELSEIF(j .eq. i+3) THEN
+					A(i,j) = An
+				ELSEIF(j .eq. i-3) THEN
+					A(i,j) = As
+				ELSE 
+					A(i,j) = 0
+				ENDIF
+			ENDIF  
+		ENDDO
+	ENDDO
+
+
+	RETURN
+	END SUBROUTINE
 
